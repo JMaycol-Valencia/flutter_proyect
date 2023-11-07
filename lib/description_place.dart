fc0abfd9388b765ce3d85e7c05c1a802ab691b31
JMaycol-Valencia <maycolvalencia50@gmail.com>
2023-11-07 09:43:49 -0400

    column, tipografias, constructor, otros

---
+++ lib/description_place.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+  //VARIABLES QUE USAREMOS
+  late String namePLace;
+  late int stars;
+  late String descriptionPlace;
+
+  //CONSTRUCTOR QUE RECIBIRA LAS VARIABLES
+  DescriptionPlace(this.namePLace, this.stars, this.descriptionPlace);
+
+  String descriptionDummy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n\nDonec auctor, nisl vitae tincidunt tincidunt, nunc nisl ultricies nunc, vitae ultricies nisl nisl vitae nisl. Donec auctor, nisl vitae tincidunt tincidunt, nunc nisl ultricies nunc, vitae ultricies nisl nisl vitae nisl.";
+
+  @override
+  Widget build(BuildContext context) {
+
+    final star_border = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: const Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_half = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: const Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: const Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final description = Column(
+      children: <Widget>[
+        Container(
+      margin: const EdgeInsets.only(
+        top: 20.0,
+        right: 20.0,
+        left: 20.0
+      ),
+      child:  Text(
+        descriptionPlace, 
+      style: const TextStyle(
+        fontFamily: "Lato",
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF56575a)
+      ))
+    )
+      ],
+    ); 
+
+
+    final titleStars = Row(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child:  Text(
+            namePLace,
+            style: const TextStyle(
+              fontFamily: "Lato",
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+            textAlign: TextAlign.left,
+          )
+        ), Row( //NECESARIO PARA LA 1 FORMA
+          children: [
+            star,star,star,star_border,star_border
+          ],
+        )
+      ],
+    );
+
+  //1 forma de ordenar los widgets
+    return Column(
+      children: [
+        titleStars,
+        description
+      ],
+    );
+    //2 forma de conseguir un buen resultado
+    // return Column(
+    //   children: [
+    //     Row( children: [
+    //       titleStars,
+    //       star,star,star,star,star
+    //     ],),
+    //     Column( children: [
+    //       description
+    //     ],)
+    //   ],
+    // );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,26 @@
 import 'package:flutter/material.dart';
+import 'description_place.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hola mundo',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey
+        primarySwatch: Colors.blueGrey,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Practica'),
         ),
-        body: Stack(
-          children: <Widget>[
-            Container(
-              width: MediaQuery. of(context). size. width,
-              height: MediaQuery. of(context). size. height,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage('https://i.pinimg.com/originals/1c/02/20/1c022001c80c4d0d2a13da2137ca56ff.jpg'),
-                  fit: BoxFit.cover
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                width: MediaQuery. of(context). size. width,
-                height: 100,
-                color: Colors.black.withOpacity(0.5),
-                child: const Center(
-                  child: Text(
-                    'Hola Mundo',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-       ),//const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-
-
-//relleno de flutter
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        body: DescriptionPlace("Bahamas", 4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n\nDonec auctor, nisl vitae tincidunt tincidunt, nunc nisl ultricies nunc, vitae ultricies nisl nisl vitae nisl. Donec auctor, nisl vitae tincidunt tincidunt, nunc nisl ultricies nunc, vitae ultricies nisl nisl vitae nisl."),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
